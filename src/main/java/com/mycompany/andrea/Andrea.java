53648a1a72bceea40f3ddc4f5512ca7d1063bf1f
ShellDomenic <domenicaloor177@gmail.com>
2025-01-08 15:18:23 -0500

    hola

---
+++ src/main/java/com/mycompany/andrea/Andrea.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.andrea;
+
+public class Andrea {
+
+    public static void main(String[] args) {
+        System.out.println("te amito");
+        System.out.println("te quelo");
+        
+    }
+}
